48f797c329f43a09d8ed5d1241acad9c0f8835a4
Uczen <uczen@dzdzw.local>
2026-03-06 11:20:02 +0100

    final

--- index.php
+++ index.php
@@ -27,60 +27,50 @@
     <main>
         <section id="blok_lewy">
             <h2>Personel</h2>
-            <form action="">
-                <input type="radio" name="radio" id="policjant" value="policjant" checked>
+            <form method="POST">
+                <input type="radio" name="status" id="policjant" value="policjant" checked>
                 <label for="policjant">Policjant</label>
-                <input type="radio" name="radio" id="ratownik" value="ratownik">
+                <input type="radio" name="status" id="ratownik" value="ratownik">
                 <label for="ratownik">Ratownik</label>
-                <button id="select">Pokaż</button>
+                <button id="select" type="submit">Pokaż</button>
             </form>
-            <h1>Wybrano opcje:</h1>
-            <div id="teble_center">
-                <table>
-                    <tr>
-                        <th>id</th>
-                        <th>Imie</th>
-                        <th>Nazwisko</th>
-                    </tr>
-                    <?php
-                    if (isset($_POST['Osoba'])){
-                        $query = 'SELECT personel.id, personel.imie, personel.nazwisko FROM personel WHERE personel.status = "policjant"';
-                        $result = mysqli_query($connection, $query);
+
+            <?php
+                if (isset($_POST['status'])) {
+                    $opcje = $_POST['status'];
+
+                    echo "<h2>Wybrano opcje: {$opcje}</h2>";
+
+                    echo "<div id='teble_center'>";
+                    echo "<table>";
+                    echo "<tr>";
+                    echo "<th>id</th>";
+                    echo "<th>Imie</th>";
+                    echo "<th>Nazwisko</th>";
+                    echo "</tr>";
+
+                    $query = "SELECT personel.id, personel.imie, personel.nazwisko FROM personel WHERE personel.status = '{$opcje}'";
+                    $result = mysqli_query($connection, $query);
+                    while ($table = mysqli_fetch_row($result)) {
+                        echo "<tr>";
+                        echo "<td>$table[0]</td>";
+                        echo "<td>$table[1]</td>";
+                        echo "<td>$table[2]</td>";
+                        echo "</tr>";
 
 
-                        for ($i = 0; $i < mysqli_num_rows($result); $i++) {
-                            $table = mysqli_fetch_row($result);
-
-                            echo "<tr>";
-                            echo "<th>$table[0]</th>";
-                            echo "<th>$table[1]</th>";
-                            echo "<th>$table[2]</th>";
-                            echo "</tr>";
-                        }
-                    } else {
-                        $query = 'SELECT personel.id, personel.imie, personel.nazwisko FROM personel WHERE personel.status = "ratownik"';
-                        $result = mysqli_query($connection, $query);
-
-
-                        for ($i = 0; $i < mysqli_num_rows($result); $i++) {
-                            $table = mysqli_fetch_row($result);
-
-                            echo "<tr>";
-                            echo "<th>$table[0]</th>";
-                            echo "<th>$table[1]</th>";
-                            echo "<th>$table[2]</th>";
-                            echo "</tr>";
-                        }
                     }
-                    ?>
-                </table>
-            </div>
+                    echo "</div>";
+                }
+            ?>
+            </table>
+
         </section>
         <section id="blok_prawy">
             <h2>Nowe zgloszenie</h2>
             <ol>
                 <?php
-                $query = 'SELECT personel.id, personel.nazwisko FROM personel WHERE personel.id NOT IN (SELECT rejestr.id_personel FROM rejestr)';
+                $query = "SELECT personel.id, personel.nazwisko FROM personel WHERE personel.id NOT IN (SELECT rejestr.id_personel FROM rejestr)";
                 $result = mysqli_query($connection, $query);
 
                 for ($i = 0; $i < mysqli_num_rows($result); $i++) {
@@ -92,13 +82,21 @@
                     echo "</ol>";
                 }
 
-                $query = mysqli_close($connection);
                 ?>
             </ol>
-            <form action="">
+            <form method="POST">
                 <label for="inp">Wybierz id osoby:</label>
-                <input type="number" id="inp">
+                <input type="number" id="inp" name="user">
                 <button>Dodaj zgloszenie</button>
+
+                <?php
+                if (isset($_POST['user'])) {
+                    $id = $_POST['user'];
+                    $query = "INSERT INTO rejestr(data, id_personel, id_pojazd) VALUES (CURRENT_DATE, '{$id}', 14);";
+                    $result = mysqli_query($connection, $query);
+                }
+                $query = mysqli_close($connection);
+                ?>
             </form>
         </section>
     </main>
